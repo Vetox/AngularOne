3e4de62e6918e99cc6a6e09cb0fcc7bc7ed16a65
Vetox <karthikeyan1997@gmail.com>
2020-05-21 00:43:04 +0530

    =initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularOne';
+
+  name = 'Karthikeyan M';
+
+  profileImg = "assets/profileIcon.png";
+  faceIcon = "assets/faceIcon.png";
+  skillsIcon = "assets/skillsIcon.png";
+
+  description = "My name is Karthikeyan. Inonvative optimized solution seeker. Excited to be at the development phase of my new career. I am ambitious, adventurous and other nice adjectives.";
+
+  Profile = {
+    name: 'Karthikeyan M',
+    skillOne: {
+      name: 'Java',
+      score: 90
+    },
+    skillTwo: {
+      name: 'Javascript',
+      score: 75
+    },
+    skillThree: {
+      name: 'CSS',
+      score: 50
+    },
+    skillFour: {
+      name: 'Angular',
+      score: 40
+    }
+  };
+
+  addSkills = {
+    one: {
+      name: "Project Management",
+      score: 25
+    },
+    two: {
+      name: "Recruitment",
+      score: 30
+    },
+    three: {
+      name: "editing",
+      score: 75
+    },
+    four: {
+      name: "Fundraising",
+      score: 60
+    }
+  };
+
+  experience = [
+    { company: "Company 1", description: "Description of work at company 1" },
+    { company: "Company 2", description: "Description of work at company 2" },
+    { company: "Company 3", description: "Description of work at company 3" },
+    { company: "Company 4", description: "Description of work at company 4" }
+  ];
+
+  education = [
+    { cert: "BTech Information Technology", description: "Description of College" },
+    { cert: "Higher Secondary School", description: "Description of 12th School" },
+    { cert: "SSLC", description: "Description of 10th School" }
+  ];
+
+  projects = [
+    {where:"BTech Major Project", description:"Description of College Major Project"},
+    {where:"School Project", description:"Description of Science Fair Project"}
+  ];
+
 }
